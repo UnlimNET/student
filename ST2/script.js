fd5a4744b9f2c03f8340958021679e31e663d9b3
Masha1174 <mmmwww93@mail.ru>
2016-08-29 11:47:18 +0300

    Add files via upload

--- ST2/script.js
+++ ST2/script.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
-    var $getWeatherBtn = $('#getWeatherBtn');
-    var $hideWeatherBtn = $('#hideWeatherBtn');
-    var $city = $('#city');
+	var $getWeatherBtn = $('#getWeatherBtn');
+	var $hideWeatherBtn = $('#hideWeatherBtn');
+	var $city = $('#city');
+	$hideWeatherBtn.hide();
 
-    $getWeatherBtn.click(function() { 	
-	    getWeather();
+	$getWeatherBtn.click(function() { 	
+		getWeather();
 	});
 
 	$hideWeatherBtn.click(function() {
@@ -14,21 +15,20 @@ $( document ).ready(function() {
 	});
 
 	function getWeather() {
-		$city.show();
-		$.ajax({
-	    	type:'get',
-		    url: 'http://api.openweathermap.org/data/2.5/forecast/city?id=484907&APPID=48e2eea04127dfd25cc4f863700757cd',             // указываем URL и
-		    dataType: 'json',                     
-		    success: function (data) { 
-	             var cityName = data.city.name;
-	             $city.empty().append('<div> ' + cityName + '</div>');
-	             var temperature = (data.list[2].main.temp - 273).toFixed(0);
-	             $city.append('<div> Средняя температура: ' + temperature + ' C </div>');
-	             $getWeatherBtn.hide();
-	             $city.addClass('zoomIn');
-	             $hideWeatherBtn.show();
-
-		    }    
-	 	});
+			$city.show();
+			$.ajax({
+			type:'get',
+			url: 'http://api.openweathermap.org/data/2.5/forecast/city?id=484907&APPID=48e2eea04127dfd25cc4f863700757cd',             // указываем URL и
+			dataType: 'json',                     
+			success: function(data) { 
+			var cityName = data.city.name;
+			$city.empty().append('<div> ' + cityName + '</div>');
+			var temperature = (data.list[2].main.temp - 273).toFixed(0);
+			$city.append('<div>Средняя температура: ' + temperature + ' C </div>');
+			$getWeatherBtn.hide();
+			$city.addClass('zoomIn');
+			$hideWeatherBtn.show();
+			}    
+		});
 	}
 })
